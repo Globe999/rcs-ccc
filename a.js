d7e7df88090a2a4dba50dd45bda1d743c7f4b070
Globe999 <Globe999@users.noreply.github.com>
2016-03-11 16:51:51 +0100

    Create a.js

---
+++ a.js
@@ -0,0 +1,45 @@
+{
+    "room": "TheArchon",
+    "author": "Globe_999",
+    "icon": "http://puu.sh/nCL60/d4d23f8aa4.jpg",
+    "css": null,
+    "rules": {
+        "allowAutorespond": true,
+        "allowAutowoot": true,
+        "allowAutojoin": false,
+        "allowAutograb": true,
+        "allowSmartVote": true,
+        "allowEmotes": true,
+        "allowShowingMehs": true,
+        "forceSmartVote": false
+    },
+    "ccc": {
+        "admin": null,
+        "ambassador": null,
+        "host": "#001AFF",
+        "cohost": "#001AFF",
+        "manager": "#1EB200",
+        "bouncer": "#CE0000",
+        "rdj": "#E6FF00",
+        "subscriber": "#D5A800",
+        "silversubscriber": "#A1A6B3",
+        "friend": "#8900FF",
+        "user": null
+    },
+    "images": {
+        "background": null,
+        "playback": null,
+        "admin": null,
+        "ambassador": null,
+        "host": null,
+        "cohost": null,
+        "manager": null,
+        "bouncer": null,
+        "rdj": null,
+        "subscriber": null,
+        "silversubscriber": "https://cdn.radiant.dj/rcs/icons/radiant/subscriber.png",
+    },
+    "emotes": {
+    	"emote_name": null
+    }
+}
